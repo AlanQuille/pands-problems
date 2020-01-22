09db78418a5ab933964fb8c99f65e45ccfdf7909
Alan Quille <malanquille@gmail.com>
2020-01-22 15:47:30 +0000

    Adding bmi.py, the weekly exercises

---
+++ bmi.py
@@ -0,0 +1,16 @@
+# weight in kg
+weight = float(input("Enter weight: "))
+# height in cm
+height = float(input("Enter height: "))
+
+# convert from cm to m
+height = height/100
+
+# get m^2 instead of m
+height = height ** 2
+
+# round to 2 decimal places
+bmi= round(weight/height, 2)
+
+#print to screen
+print("BMI is", bmi)
