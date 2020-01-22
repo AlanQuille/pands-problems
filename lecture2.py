e4bf4662926fda74dcafae2f28efb8a3d8721f4f
Alan Quille <malanquille@gmail.com>
2020-01-22 15:31:13 +0000

    Added new file for calculating tiles.

---
+++ lecture2.py
@@ -0,0 +1,15 @@
+
+length = float(input("Enter room length: "))
+width = float(input("Enter room width: "))
+
+
+# length = 3.5
+# width = 2.3
+
+area = length * width
+
+# needed is not a good variable name
+#1.05 is a literal value i.e. a specified value
+needed = area * 1.05
+
+print("You need", needed, "tiles in metres squared.")
