c5806730da5ebe4379dc0e759ebbf33ffabe381f
Alan Quille <malanquille@gmail.com>
2020-01-30 14:28:30 +0000

    Weekly task 3, secondstring.py, adding comments

--- secondstring.py
+++ secondstring.py
@@ -1,5 +1,9 @@
+# Get input and convert it to string
 input_string = str(input("Please enter a sentence: "))
 
+# Take the last letter, going to the first letter by
+# by every second letter
 output_string = input_string[-1:0:-2]
 
+# Print the resulting string
 print(output_string)
