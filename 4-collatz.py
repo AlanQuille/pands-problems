adf5af6e6dfd608a392a422b47e1ddf875f54b5a
Alan Quille <malanquille@gmail.com>
2020-02-05 16:55:05 +0000

    Fixing weekly task numbers for code files, should be 2 3 4 not 1 2 3

--- 4-collatz.py
+++ 4-collatz.py
@@ -7,16 +7,20 @@ if any_pos_int <= 0:
     # input
     print("The integer you have entered is not positive.")
 else:
+    # print out what the user input for any_pos_int
+    # end =' ' ensures that it ends with a space
+    # not newline
+    print(any_pos_int, end=' ')
     # the while loop will end if any_pos_int is less than or equal to 1
     while(any_pos_int > 1):
-        # this prints any_pos_int and formats it as shown in
-        # the example output
-        print(any_pos_int, end=' ')
         # if 2 divides into any_pos_int, divide it by two
         if(any_pos_int % 2 == 0):
             # also converts it to integer so it is not float
             any_pos_int = int(any_pos_int/2)
-            # else (it is odd) multiply by three and add one
+        # else (it is odd) multiply by three and add one
         else:
-            # also converts it to  integer so it is not a float
+            # also converts it to integer so it is not a float
             any_pos_int = int((any_pos_int * 3) + 1)
+        # this prints any_pos_int and formats it as shown in
+        # the example output
+        print(any_pos_int, end=' ')
