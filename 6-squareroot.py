66bb15ae01ab134ca43ca629b3144ef6bc44b218
Alan Quille <malanquille@gmail.com>
2020-02-19 17:26:16 +0000

    Adding 6-squareroot.py and functions.py

---
+++ 6-squareroot.py
@@ -0,0 +1,17 @@
+
+# This imports the newton_root function from the module functions.py
+from functions import newton_root
+
+# This takes in input from the user, converts 
+# it to float and gets the absolute value so that 
+# strings will not be accepted and it will
+# be strictly positive
+pos= abs(float(input("Please enter a positive number: ")))
+
+# This applies the Newton_Raphson method for finding square roots
+# for 10 iterations (should be sufficiently accurate for 1 decimal place)
+# and rounds the result to 1 decimal place using the round function
+pos = round(newton_root(pos, 10), 1)
+
+# This prints the result to the screen
+print("The square root of 14.5 is approx. {}".format(pos))
---
+++ functions.py
@@ -0,0 +1,24 @@
+# This function implements "iterations" steps of the Newton-Raphson method
+# for finding roots. In particular, it finds the roots of the equation
+# x^2 - input, which is identical to asking what is the square 
+# root of the input. The derivative of x^2 - input is 2x
+def newton_root(input, iterations):
+    # Rough guess of answer for Newton-Raphson
+    init = input/4
+    # This variable i is used in the while loop 
+    i=0
+
+    # This executes a while loop until i reaches iterations
+    while i<iterations:
+        # This is the formula for the Newton-Raphson method for finding
+        # square roots
+        init = init - ((init**2 - input)/(2*init))
+        # Without this line of code, the loop would never reach iterations
+        i = i +1
+
+    return init
+    
+
+
+
+
