79dab1dcd028e4eabd5a52469bfbb26b55d344c1
Alan Quille <malanquille@gmail.com>
2020-02-19 18:19:18 +0000

    Eliminate unnecessary whitespace in functions.py

--- functions.py
+++ functions.py
@@ -7,7 +7,6 @@ def newton_root(input, iterations):
     init = input/4
     # This variable i is used in the while loop 
     i=0
-
     # This executes a while loop until i reaches iterations
     while i<iterations:
         # This is the formula for the Newton-Raphson method for finding
@@ -15,7 +14,7 @@ def newton_root(input, iterations):
         init = init - ((init**2 - input)/(2*init))
         # Without this line of code, the loop would never reach iterations
         i = i +1
-
+        
     return init
     
 
